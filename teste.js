e231b450fcd5d063ec220f475942e1cf0b9bd32d
vhshinigami77 <61999227+vhshinigami77@users.noreply.github.com>
2025-09-01 01:39:08 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,4 +1,3 @@
-// backend.js
 import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
@@ -8,7 +7,7 @@ import { execSync } from 'child_process';
 import { fileURLToPath } from 'url';
 
 const app = express();
-app.use(cors()); // Permite requisições cross-origin
+app.use(cors());
 const upload = multer({ dest: 'uploads/' });
 
 const __filename = fileURLToPath(import.meta.url);
@@ -49,9 +48,9 @@ app.post('/upload', upload.single('audio'), async (req, res) => {
     // ========================
     const windowSize = sampleRate; // 1 segundo
     const N = Math.min(windowSize, int16Samples.length);
-    const freqStep = 0.5;        // maior resolução
-    const minFreq = 261;          // C4
-    const maxFreq = 1175;         // D6
+    const freqStep = 2;
+    const minFreq = 16;
+    const maxFreq = 1048;
 
     let maxMag = 0;
     let peakFreq = 0;
