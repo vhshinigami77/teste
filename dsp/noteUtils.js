90bc4898358b79dd51eabaa76784bde1cf825c09
vhshinigami77 <61999227+vhshinigami77@users.noreply.github.com>
2026-01-27 01:31:15 -0300

    Create noteUtils.js

---
+++ dsp/noteUtils.js
@@ -0,0 +1,12 @@
+export function frequencyToNote(freq) {
+  if (!freq || freq < 20) return 'PAUSA';
+
+  const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F',
+                     'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+  const midi = Math.round(69 + 12 * Math.log2(freq / 440));
+  const note = noteNames[midi % 12];
+  const octave = Math.floor(midi / 12) - 1;
+
+  return ${note}${octave};
+}
