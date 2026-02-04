364e10a546412f746357bbc5f5ccf5f1732ad513
vhshinigami77 <61999227+vhshinigami77@users.noreply.github.com>
2026-02-04 00:30:09 -0300

    Update noteUtils.js

--- dsp/noteUtils.js
+++ dsp/noteUtils.js
@@ -4,7 +4,13 @@ export function frequencyToNote(freq) {
   const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F',
                      'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
-  const midi = Math.round(69 + 12 * Math.log2(freq / 440));
+  const midiFloat = 69 + 12 * Math.log2(freq / 440);
+  const midi = Math.round(midiFloat);
+
+  // margem de segurança ±25 cents
+  const cents = (midiFloat - midi) * 100;
+  if (Math.abs(cents) > 25) return 'PAUSA';
+
   const note = noteNames[midi % 12];
   const octave = Math.floor(midi / 12) - 1;
 
